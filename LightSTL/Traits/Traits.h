af0e7b3fbb696d339c514bbb9813b10d6a63c8fc
gzwl <mrweiliangzhang@gmail.com>
2015-07-31 20:14:10 +0800

    完善traits技法

--- LightSTL/Traits/Traits.h
+++ LightSTL/Traits/Traits.h
@@ -2,101 +2,150 @@
 #define TRAITS_H
 
 namespace LightSTL{
-}
-struct trival_type{};
-struct nontrival_type{};
+
+/***************iterator_traits，用来萃取迭代器的性质***************/
+
+struct normal_iterator{};
+struct forward_iterator : public normal_iterator{};
+struct bidirection_iterator : public forward_iterator{};
+struct random_access_iterator : public bidirection_iterator{};
+
+template<class Iterator>
+struct iterator_traits
+{
+	typedef typename Iterator::iterator_type iterator_type;
+	typedef typename Iterator::value_type value_type;	
+};
+
+template<class T>
+struct iterator_traits<T*>
+{
+	typedef random_access_iterator iterator_type;
+    typedef T value_type;	
+};
+
+/***************type_traits，用来萃取类的性质***************/
+
+struct true_type{};
+struct false_type{};
 
 template<class T>
 struct type_traits
 {
-	typedef nontrival_type destructor_type; 
-}
+	typedef false_type has_trival_destructor_type;
+	typedef false_type has_trival_assignment_type;
+   	typedef false_type is_POD_type;	
+};
 
 
 
 template<class T>
 struct type_traits<T*>
 {
-	typedef trival_type destructor_type;
-}
+	typedef true_type has_trival_destructor_type;
+	typedef true_type has_trival_assignment_type;
+	typedef true_type is_POD_type; 
+};
 
 template<>
 struct type_traits<int>
 {
-	typedef trival_type destructor_type;
-}
+	typedef true_type has_trival_destructor_type;
+	typedef true_type has_trival_assignment_type;
+	typedef true_type is_POD_type; 
+};
 
 
 template<>
 struct type_traits<unsigned int>
 {
-	typedef trival_type destructor_type;
-}
+	typedef true_type has_trival_destructor_type;
+	typedef true_type has_trival_assignment_type;
+	typedef true_type is_POD_type; 
+};
 
 
 template<>
 struct type_traits<short>
 {
-	typedef trival_type destructor_type;
-}
+	typedef true_type has_trival_destructor_type;
+	typedef true_type has_trival_assignment_type;
+	typedef true_type is_POD_type; 
+};
 
 
 template<>
 struct type_traits<unsigned short>
 {
 
-	typedef trival_type destructor_type;
-}
+	typedef true_type has_trival_destructor_type;
+	typedef true_type has_trival_assignment_type;
+	typedef true_type is_POD_type; 
+};
 
 
 template<>
 struct type_traits<long>
 {
-	typedef trival_type destructor_type;
-}
+	typedef true_type has_trival_destructor_type;
+	typedef true_type has_trival_assignment_type;
+	typedef true_type is_POD_type; 
+};
 
 
 template<>
 struct type_traits<unsigned long>
 {
-	typedef trival_type destructor_type;
-}
+	typedef true_type has_trival_destructor_type;
+	typedef true_type has_trival_assignment_type;
+	typedef true_type is_POD_type; 
+};
 
 
 template<>
 struct type_traits<char>
 {
-	typedef trival_type destructor_type;
-}
+	typedef true_type has_trival_destructor_type;
+	typedef true_type has_trival_assignment_type;
+	typedef true_type is_POD_type; 
+};
 
 
 template<>
 struct type_traits<unsigned char>
 {
-	typedef trival_type destructor_type;
-}
+	typedef true_type has_trival_destructor_type;
+	typedef true_type has_trival_assignbment_type;
+	typedef true_type is_POD_type; 
+};
 
 
 template<>
 struct type_traits<float>
 {
-	typedef trival_type destructor_type;
-}
+	typedef true_type has_trival_destructor_type;
+	typedef true_type has_trival_assignment_type;
+	typedef true_type is_POD_type; 
+};
 
 
 template<>
 struct type_traits<double>
 {
-	typedef trival_type destructor_type;
-}
+	typedef true_type has_trival_destructor_type;
+	typedef true_type has_trival_assignment_type;
+	typedef true_type is_POD_type; 
+};
 
 
 template<>
 struct type_traits<long double>
 {
-	typedef trival_type destructor_type;
+	typedef true_type has_trival_destructor_type;
+	typedef true_type has_trival_assignment_type;
+	typedef true_type is_POD_type; 
+};
+
 }
-
-
 
 #endif
